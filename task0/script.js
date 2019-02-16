7e6e25306296c8fece8ff8376ed383947146790d
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-02-16 11:23:27 +0100

    rendering

--- task0/script.js
+++ task0/script.js
@@ -3,11 +3,19 @@ class Users {
   constructor(containerId) {
     this.container = document.getElementById(containerId);
 
+    this.users = {
+      results: [{
+        name: 'Jan',
+        surname: 'Kowalski'
+      }]
+    };
+
     this.init();
   }
 
   init() {
-
+    // this.fetchUsers()
+    this.render();
   }
 
   fetchUsers() {
@@ -16,8 +24,20 @@ class Users {
   }
 
   render() {
-
+    const ul = document.createElement('ul');
+    this.users.results.forEach(elem => {
+      const li = document.createElement('li');
+      li.innerHTML = `${elem.name} ${elem.surname}`;
+      ul.appendChild(li);
+    });
+    this.container.appendChild(ul);
   }
+  // JSX
+  // <ul>
+  //   {this.users.results.map(elem) => (
+  //     <li>{elem.name} {ele.surname}</li>
+  //   )}
+  // </ul>
 }
 
 const list = new Users('root');
