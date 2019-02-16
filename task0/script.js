fbdc741be414c3620bcf8a075ccbeed686f5c364
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-02-16 11:10:34 +0100

    users list class - step 1

---
+++ task0/script.js
@@ -0,0 +1,23 @@
+class Users {
+
+  constructor(containerId) {
+    this.container = document.getElementById(containerId);
+
+    this.init();
+  }
+
+  init() {
+
+  }
+
+  fetchUsers() {
+
+    // this.render();
+  }
+
+  render() {
+
+  }
+}
+
+const list = new Users('root');
